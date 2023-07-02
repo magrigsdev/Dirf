db7f6c5e94cac7cc91c6f2780756c069f7f7711f
fridev <96908551+magrigsdev@users.noreply.github.com>
2023-07-03 01:53:11 +0200

    adding picture

--- dirf_restaurant/src/components/Restaurant.js
+++ dirf_restaurant/src/components/Restaurant.js
@@ -3,7 +3,26 @@ import Header from './Header'
 
 function Restaurant() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <div class="container text-center">
+          <div class="row">
+            <div class="col">
+              Column
+            </div>
+
+            <div class="col">
+              Column
+            </div>
+            
+            <div class="col">
+              Column
+            </div>
+          </div>
+      </div>
+    </>
+    
+
   )
 }
 
