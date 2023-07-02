f039556c2cb15fde7060d6d766e7db766525514d
fridev <96908551+magrigsdev@users.noreply.github.com>
2023-07-03 01:10:22 +0200

    header done

--- dirf_restaurant/src/components/Header.js
+++ dirf_restaurant/src/components/Header.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import Navbar from './Navbar'
+import LE_GOURMET_DE_SEZE  from "../assets/video/LE_GOURMET_DE_SEZE.mp4";
+import MyHeader from "../assets/css/MyHeader.css";
 
 function Header() {
   return (
    
     <>
       <Navbar/>
-      <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-bs-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="..." class="d-block w-100" alt="..."/>
+      <div className='container-fluid'>
+            <div className='row w-auto myvideo'>       
+               <video src={LE_GOURMET_DE_SEZE}  autoPlay loop muted/>
             </div>
 
-            <div class="carousel-item">
-              <img src="..." class="d-block w-100" alt="..."/>
+            <div className='row w-auto mytext text-left p-2 text-white'> 
+              <div className='col-*-12 align-self-start'>
+              <h1 class="h1">Dirf Restaurant</h1>
+              </div>
+              <div className='col-*-12 align-self-start'>
+                <h1 class="h4 text-gray-500 ">Une cuisine proche du produit, inspirée par la nature et le terroir</h1>
+              </div>       
+              <div className='col align-self-start'>
+              <button type="button" class="btn btn-danger">Voir le menu</button>
+              </div> 
             </div>
-
-            <div class="carousel-item">
-              <img src="..." class="d-block w-100" alt="..."/>
-            </div>
-          </div>
       </div>
     </>
   )
