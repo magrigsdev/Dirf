2ae4fb98dbbfb2f4fcd5184a6a3b97419e5bdaa2
fridev <96908551+magrigsdev@users.noreply.github.com>
2023-07-03 18:03:56 +0200

    finie

--- dirf_restaurant/src/data/Pictures.js
+++ dirf_restaurant/src/data/Pictures.js
@@ -1,5 +1,7 @@
 
+ 
 import pic2 from '../assets/pictures/pic2.jpg' 
+ 
 import React from 'react'
 
 
@@ -10,13 +12,13 @@ function Pictures() {
         <div class="container text-center">
             <div className='row'>
                 <div className="col-sm-4">
-                    <img src={pic2} alt="Imge 1" className="img-fluid img-thumbnail" />
+                    <img src={pic2} alt="Imge 1" className="img-fluid img-thumbnail  " />
                 </div>
                 <div className="col-sm-4">
-                    <img src={pic2} alt="Imge 2" className="img-fluid img-thumbnail" />
+                    <img src={pic2} alt="Imge 2" className="img-fluid img-thumbnail " />
                 </div>
                 <div className="col-sm-4">
-                    <img src={pic2} alt="Imge 3" className="img-fluid img-thumbnail" />
+                    <img src={pic2} alt="Imge 3" className="img-fluid img-thumbnail " />
                 </div>
             </div> 
       </div>
