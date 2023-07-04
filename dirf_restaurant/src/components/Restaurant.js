279320bd59e0f99819f22f677de71615299d8c8c
fridev <96908551+magrigsdev@users.noreply.github.com>
2023-07-04 10:37:00 +0200

    chef done

--- dirf_restaurant/src/components/Restaurant.js
+++ dirf_restaurant/src/components/Restaurant.js
@@ -2,12 +2,25 @@ import React from 'react'
 import Header from './Header'
 import Pictures from '../data/Pictures'
 
+import pic5 from '../assets/pictures/pic5.png'
+
 
 
 
 
 function Restaurant() {
- 
+
+
+  const mycol = {
+    marginLeft:'20px'
+  }
+
+  const chef = {marginTop:'100px'}
+
+
+
+
+
   return (
     <>
       <Header/>
@@ -16,6 +29,52 @@ function Restaurant() {
         Restaurant gastronomique étoilé au guide Michelin depuis 2002
         </p>
       <Pictures/>
+
+      <div class="container text-center">
+        <div className='row my-10'>
+        <p className='fs-5'><em>Situé au coeur du 6ème arrondissement de Lyon, le restaurant gastronomique Le Gourmet de Sèze vous accueille dans une ambiance sereine et intemporelle.
+            Le temps d'un repas, laissez-vous transporter dans l'univers culinaire de Bernard Mariller.</em></p>
+        </div>
+      </div>
+
+      <div class="container" style={chef}>
+        <div class="row py-10">
+          <div class="col">
+          <img src="https://images8.alphacoders.com/818/thumbbig-818929.webp" class="img-thumbnail" alt="..."/>
+          </div>
+
+          <div class="col" style={mycol}>
+            <h1 class="display-5">Nico kovnovitch</h1>
+            <p class="lead">
+              Chef étoilé depuis 2002 au guide Michelin, Nico Kovnovitch est également membre des Toques Blanches Lyonnaises.
+              Le  chef et son équipe partageront avec vous leur passion pour leur métier.
+              Avec un service soigné et discret, ils sauront faire de ce moment une expérience gastronomique unique.
+            </p>            
+            <button type="button" class="btn btn-outline-success d-flex justify-content-center">En savoir plus du chef</button>            
+          </div>
+
+        </div>
+      </div>
+
+      <div class="container" style={chef}>
+        <div class="row py-10">
+          
+          <div class="col">
+            <h1 class="display-6">Notre chef recommande</h1>
+            <p class="lead">
+            Le poulet rôti est un plat classique et délicieux qui fait saliver les papilles. 
+            Lorsqu'il est préparé avec soin, le poulet rôti est juteux à l'intérieur et croustillant à l'extérieur, ce qui en fait un repas réconfortant
+             et savoureux pour de nombreuses personnes.
+            </p>            
+            <button type="button" class="btn btn-lg btn-outline-danger d-flex justify-content-center">Menus</button>            
+          </div>
+
+          <div class="col">
+            <img src= {pic5} class="img-thumbnail" alt="..."/>
+          </div>
+
+        </div>
+      </div>
 
     </>
     
--- dirf_restaurant/src/data/Pictures.js
+++ dirf_restaurant/src/data/Pictures.js
@@ -1,6 +1,6 @@
-
- 
 import pic2 from '../assets/pictures/pic2.jpg' 
+import pic3 from '../assets/pictures/pic3.jpg'  
+import pic4 from '../assets/pictures/pic4.jpg' 
  
 import React from 'react'
 
@@ -15,13 +15,13 @@ function Pictures() {
                     <img src={pic2} alt="Imge 1" className="img-fluid img-thumbnail  " />
                 </div>
                 <div className="col-sm-4">
-                    <img src={pic2} alt="Imge 2" className="img-fluid img-thumbnail " />
+                    <img src={pic3} alt="Imge 2" className="img-fluid img-thumbnail " />
                 </div>
                 <div className="col-sm-4">
-                    <img src={pic2} alt="Imge 3" className="img-fluid img-thumbnail " />
+                    <img src={pic4} alt="Imge 3" className="img-fluid img-thumbnail " />
                 </div>
             </div> 
-      </div>
+        </div>
    </>
   )
 }
